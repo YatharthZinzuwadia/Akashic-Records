b6d7dd501c4b42d5a2d41dba32e7b1197f0db1cb
YatharthZinzuwadia <yatharthzinzuwadia@gmail.com>
2024-09-21 13:31:53 +0530

    deploy

---
+++ src/components/Footer.jsx
@@ -0,0 +1,21 @@
+// import React from "react";
+
+function Footer() {
+  return (
+    <div
+      style={{
+        backgroundColor: "black",
+        color: "white",
+        alignItems: "center",
+        textAlign: "center",
+        fontSize: "1rem",
+        padding: "15px",
+      }}
+    >
+      &copy; All Rights reserved | Designed and Developed by Yatharth Zinzuwadia
+      | 2024
+    </div>
+  );
+}
+
+export default Footer;
